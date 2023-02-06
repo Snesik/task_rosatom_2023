1634cc6a1958e7df5c03da40fdfec6ee73e065f0
Snesik <lightprince23@yandex.ru>
2023-02-06 23:03:54 +0300

    - 2 готово

--- task/2_task.py
+++ task/2_task.py
@@ -1,19 +1,22 @@
 """
 Какие ты видишь проблемы в следующем фрагменте кода?
+step не становится локальной переменной лямбды, а является внешней глобальной переменной
+
 Как его следует исправить?
+нужно в лямбде значение задавать как параметр со значением
+
 Исправь ошибку и перепиши код ниже с использованием типизации
 """
 from typing import Callable
 
-def create_handlers(callback: Callable):
+
+def create_handlers(callback: Callable) -> list:
     handlers = []
     for step in range(5):
         # Добавляем обработчики для каждого шага (от 0 до 4)
-        handlers.append(lambda step=step: callback(step+1))
+        handlers.append(lambda step=step: callback(step))
     return handlers
 
-def plus(d):
-    return d + 1
 
 def execute_handlers(handlers):
     # Запускаем добавленный обработчики (шаги от 0 до 4)
@@ -21,4 +24,9 @@ def execute_handlers(handlers):
         handler()
 
 
-execute_handlers(create_handlers(plus))
+def check(number) -> int:
+    return number + 1
+
+
+if __name__ == '__main__':
+    execute_handlers(create_handlers(check))
